4bbb525dcaa7bfcd1f34cd539e692e9b03bbd2ff
ahilessprime <ahilessprime@gmail.com>
2020-08-20 17:16:04 +0300

    #2

--- Test/TestAddition.java
+++ Test/TestAddition.java
@@ -1,9 +1,13 @@
 package Test;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 import lessonTDD_multiCurrency.Dollar;
 
 public class TestAddition {
 
+    @Test
     public void testMultiplication(){
         Dollar five = new Dollar(5);
         five.times(2);
